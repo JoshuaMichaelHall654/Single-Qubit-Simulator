0f8480917911b4ecdac00905a221402bf0bca0ec
JOSH HALL <jmhall7@wisc.edu>
2026-01-15 12:56:50 -0500

    Improved text validation speed and UX

--- Frontend/qubit-frontend/src/testFunctionality.js
+++ Frontend/qubit-frontend/src/testFunctionality.js
@@ -1,1 +1,6 @@
 // test the validate input function
+1, illegal expression: []
+2, symbols not allowed: x, cos(x), a
+3, disallowed function: arccos(1), setUnion()
+4, function with too many or too little argumens: complex(1, 5, 6), acos(1, 2), sin(1,2,3,4,5,6*1)
+5, unfinished expression: 1 *, 4^, cos(, / 2
