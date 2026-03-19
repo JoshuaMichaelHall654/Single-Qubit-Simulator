6f47fdd8dbef938fcd9ef6ee8ef8661e3d66711b
JOSH HALL <jmhall7@wisc.edu>
2026-03-19 13:42:09 -0400

    Merged seperate + and - tests into +/-, added some symbol tests to normalization check

--- Frontend/qubit-frontend/src/components/tests/testCheckNormalization.test.js
+++ Frontend/qubit-frontend/src/components/tests/testCheckNormalization.test.js
@@ -53,133 +53,85 @@ test("helper correctly returns norm and prob for doubles greater than our 10^-11
 /**
  * Main function tests: unnormalized inputs
  */
-test("results are unnormalized for two integers added together (1, 1, +)", () => {
+test("results are unnormalized for two integers (1, 1, +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("1", "1", true);
+  const resultPlus = checkNormalization("1", "1", true);
   // Make sure checkNormalization returns not normalized
-  expect(result.resultOfCheck).toBe("not normalized");
+  expect(resultPlus.resultOfCheck).toBe("not normalized");
+  const resultMinus = checkNormalization("1", "1", false);
+  expect(resultMinus.resultOfCheck).toBe("not normalized");
 });
 
-test("results are unnormalized for two integers subtracted (1, 1, -)", () => {
+test("results are unnormalized for one integer and one double (1, 1.0, +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("1", "1", false);
+  const resultPlus = checkNormalization("1", "1.0", true);
   // Make sure checkNormalization returns not normalized
-  expect(result.resultOfCheck).toBe("not normalized");
+  expect(resultPlus.resultOfCheck).toBe("not normalized");
+  const resultMinus = checkNormalization("1", "1.0", false);
+  expect(resultMinus.resultOfCheck).toBe("not normalized");
 });
 
-test("results are unnormalized for one integer and one double added together (1, 1.0, +)", () => {
+test("results are unnormalized for one double value and one integer (1.0, 1, +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("1", "1.0", true);
+  const resultPlus = checkNormalization("1.0", "1", true);
   // Make sure checkNormalization returns not normalized
-  expect(result.resultOfCheck).toBe("not normalized");
+  expect(resultPlus.resultOfCheck).toBe("not normalized");
+  const resultMinus = checkNormalization("1.0", "1", false);
+  expect(resultMinus.resultOfCheck).toBe("not normalized");
 });
 
-test("results are unnormalized for one integer and one double subtracted (1, 1.0, -)", () => {
+test("results are unnormalized for two double values (1.0, 1.0, +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("1", "1.0", false);
+  const resultPlus = checkNormalization("1.0", "1.0", true);
   // Make sure checkNormalization returns not normalized
-  expect(result.resultOfCheck).toBe("not normalized");
+  expect(resultPlus.resultOfCheck).toBe("not normalized");
+  const resultMinus = checkNormalization("1.0", "1.0", false);
+  expect(resultMinus.resultOfCheck).toBe("not normalized");
 });
 
-test("results are unnormalized for one double value and one integer added together (1.0, 1, +)", () => {
+test("results are unnormalized for one large double and one integer (1.00000000001, 1, +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("1.0", "1", true);
+  const resultPlus = checkNormalization("1.00000000001", "1", true);
   // Make sure checkNormalization returns not normalized
-  expect(result.resultOfCheck).toBe("not normalized");
+  expect(resultPlus.resultOfCheck).toBe("not normalized");
+  const resultMinus = checkNormalization("1.00000000001", "1", false);
+  expect(resultMinus.resultOfCheck).toBe("not normalized");
 });
 
-test("results are unnormalized for one double value and one integer subtracted (1.0, 1, -)", () => {
+test("results are unnormalized for doubles greater than our 10^-11 margin of error (.00001, 1, +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("1.0", "1", false);
+  const resultPlus = checkNormalization(".00001", "1", true);
   // Make sure checkNormalization returns not normalized
-  expect(result.resultOfCheck).toBe("not normalized");
-});
-
-test("results are unnormalized for two double values added (1.0, 1.0, +)", () => {
-  // + = true, - = false. Check normalization expects strings from
-  // the user typing
-  const result = checkNormalization("1.0", "1.0", true);
-  // Make sure checkNormalization returns not normalized
-  expect(result.resultOfCheck).toBe("not normalized");
-});
-
-test("results are unnormalized for two double values subtracted (1.0, 1.0, -)", () => {
-  // + = true, - = false. Check normalization expects strings from
-  // the user typing
-  const result = checkNormalization("1.0", "1.0", false);
-  // Make sure checkNormalization returns not normalized
-  expect(result.resultOfCheck).toBe("not normalized");
-});
-
-test("results are unnormalized for one large double added to an integer (1.00000000001, 1, +)", () => {
-  // + = true, - = false. Check normalization expects strings from
-  // the user typing
-  const result = checkNormalization("1.00000000001", "1", true);
-  // Make sure checkNormalization returns not normalized
-  expect(result.resultOfCheck).toBe("not normalized");
-});
-
-test("results are unnormalized for one large double subtracted from an integer (1.00000000001, 1, -)", () => {
-  // + = true, - = false. Check normalization expects strings from
-  // the user typing
-  const result = checkNormalization("1.00000000001", "1", false);
-  // Make sure checkNormalization returns not normalized
-  expect(result.resultOfCheck).toBe("not normalized");
-});
-
-test("results are unnormalized for doubles greater than our 10^-11 margin of error added (.00001, 1, +)", () => {
-  // + = true, - = false. Check normalization expects strings from
-  // the user typing
-  const result = checkNormalization(".00001", "1", true);
-  // Make sure checkNormalization returns not normalized
-  expect(result.resultOfCheck).toBe("not normalized");
-});
-
-test("results are unnormalized for doubles greater than our 10^-11 margin of error subtracted (.00001, 1, -)", () => {
-  // + = true, - = false. Check normalization expects strings from
-  // the user typing
-  const result = checkNormalization(".00001", "1", false);
-  // Make sure checkNormalization returns not normalized
-  expect(result.resultOfCheck).toBe("not normalized");
+  expect(resultPlus.resultOfCheck).toBe("not normalized");
+  const resultMinus = checkNormalization(".00001", "1", false);
+  expect(resultMinus.resultOfCheck).toBe("not normalized");
 });
 
 // Check normalization handles expressions, including complex numbers and functions
-test("results are unnormalized for user_test_2 added (1 + 2i, 2 + 3i, +)", () => {
+test("results are unnormalized for user_test_2  (1 + 2i, 2 + 3i, +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("1 + 2i", "2 + 3i", true);
-  // Make sure checkNormalization returns normalized
-  expect(result.resultOfCheck).toBe("not normalized");
+  const resultPlus = checkNormalization("1 + 2i", "2 + 3i", true);
+  // Make sure checkNormalization returns not normalized
+  expect(resultPlus.resultOfCheck).toBe("not normalized");
+  const resultMinus = checkNormalization("1 + 2i", "2 + 3i", false);
+  expect(resultMinus.resultOfCheck).toBe("not normalized");
 });
 
-test("results are unnormalized for user_test_2 subtracted (1 + 2i, 2 + 3i, -)", () => {
+test("results are unnormalized for user_test_3 (sin(2i^3), 3^i * 4, +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("1 + 2i", "2 + 3i", false);
-  // Make sure checkNormalization returns normalized
-  expect(result.resultOfCheck).toBe("not normalized");
-});
-
-test("results are unnormalized for user_test_3 added (sin(2i^3), 3^i * 4, +)", () => {
-  // + = true, - = false. Check normalization expects strings from
-  // the user typing
-  const result = checkNormalization("sin(2i^3)", "3^i * 4", true);
-  // Make sure checkNormalization returns normalized
-  expect(result.resultOfCheck).toBe("not normalized");
-});
-
-test("results are unnormalized for user_test_3 subtracted (sin(2i^3), 3^i * 4, +)", () => {
-  // + = true, - = false. Check normalization expects strings from
-  // the user typing
-  const result = checkNormalization("sin(2i^3)", "3^i * 4", false);
-  // Make sure checkNormalization returns normalized
-  expect(result.resultOfCheck).toBe("not normalized");
+  const resultPlus = checkNormalization("sin(2i^3)", "3^i * 4", true);
+  // Make sure checkNormalization returns not normalized
+  expect(resultPlus.resultOfCheck).toBe("not normalized");
+  const resultMinus = checkNormalization("sin(2i^3)", "3^i * 4", false);
+  expect(resultMinus.resultOfCheck).toBe("not normalized");
 });
 
 // ================================================================
@@ -208,68 +160,139 @@ test("helper correctly returns norm and prob for integers (1, 0)", () => {
 /**
  * Main function tests: normalized inputs
  */
-test("results are normalized for integers that are normalized when added (0, 1, +)", () => {
+test("results are normalized for integers that are normalized (0, 1, +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("0", "1", true);
+  const resultPlus = checkNormalization("0", "1", true);
   // Make sure checkNormalization returns normalized
-  expect(result.resultOfCheck).toBe("normalized");
+  expect(resultPlus.resultOfCheck).toBe("normalized");
+  const resultMinus = checkNormalization("0", "1", false);
+  expect(resultMinus.resultOfCheck).toBe("normalized");
 });
 
-test("results are normalized for integers that are normalized when subtracted (0, 1, -)", () => {
+test("results are normalized for integers that are normalized (1, 0, +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("0", "1", false);
+  const resultPlus = checkNormalization("1", "0", true);
   // Make sure checkNormalization returns normalized
-  expect(result.resultOfCheck).toBe("normalized");
+  expect(resultPlus.resultOfCheck).toBe("normalized");
+  const resultMinus = checkNormalization("1", "0", false);
+  expect(resultMinus.resultOfCheck).toBe("normalized");
 });
 
-test("results are normalized for integers that are normalized when added (1, 0, +)", () => {
+test("results are normalized for one double and one integer that are normalized (0.0 , 1, +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("1", "0", true);
+  const resultPlus = checkNormalization("0.0", "1", true);
   // Make sure checkNormalization returns normalized
-  expect(result.resultOfCheck).toBe("normalized");
+  expect(resultPlus.resultOfCheck).toBe("normalized");
+  const resultMinus = checkNormalization("0.0", "1", false);
+  // Make sure checkNormalization returns normalized
+  expect(resultMinus.resultOfCheck).toBe("normalized");
 });
 
-test("results are normalized for integers that are normalized when subtracted (1, 0, -)", () => {
+test("results are normalized for one double and one integer that are normalized (0, 1.0, +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("1", "0", false);
+  const resultPlus = checkNormalization("0", "1.0", true);
   // Make sure checkNormalization returns normalized
-  expect(result.resultOfCheck).toBe("normalized");
+  expect(resultPlus.resultOfCheck).toBe("normalized");
+  const resultMinus = checkNormalization("0", "1.0", false);
+  expect(resultMinus.resultOfCheck).toBe("normalized");
 });
 
-test("results are normalized for one double and one integer that are normalized and added (0.0 , 1, +)", () => {
+// List of symbols:
+/**
+  "e",
+  "E",
+  "tau",
+  "phi",
+  "SQRT1_2",
+  "SQRT2",
+ */
+test("results return normalized for states using pi use that is normalized (1/pi, sqrt(1 - 1/pi^2), +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("0.0", "1", true);
+  const resultPlus = checkNormalization("1/pi", "sqrt(1 - 1/pi^2)", true);
   // Make sure checkNormalization returns normalized
-  expect(result.resultOfCheck).toBe("normalized");
+  expect(resultPlus.resultOfCheck).toBe("normalized");
+  const resultMinus = checkNormalization("1/pi", "sqrt(1 - 1/pi^2)", false);
+  expect(resultMinus.resultOfCheck).toBe("normalized");
 });
 
-test("results are normalized for one double and one integer that are normalized and subtracted (0.0 , 1, -)", () => {
+test("results return normalized for states using PI use that is normalized (1/PI, sqrt(1 - 1/PI^2), +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("0.0", "1", false);
+  const resultPlus = checkNormalization("1/PI", "sqrt(1 - 1/PI^2)", true);
   // Make sure checkNormalization returns normalized
-  expect(result.resultOfCheck).toBe("normalized");
+  expect(resultPlus.resultOfCheck).toBe("normalized");
+  const resultMinus = checkNormalization("1/PI", "sqrt(1 - 1/PI^2)", false);
+  expect(resultMinus.resultOfCheck).toBe("normalized");
 });
 
-test("results are normalized for one double and one integer that are normalized added together (0, 1.0, +)", () => {
+test("results return normalized for states with complex values using i that is normalized ((1 + i) / 2, (1 - i) / 2, +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("0", "1.0", true);
+  const resultPlus = checkNormalization("(1 + i) / 2", "(1 - i) / 2", true);
   // Make sure checkNormalization returns normalized
-  expect(result.resultOfCheck).toBe("normalized");
+  expect(resultPlus.resultOfCheck).toBe("normalized");
+  const resultMinus = checkNormalization("(1 + i) / 2", "(1 - i) / 2", false);
+  expect(resultMinus.resultOfCheck).toBe("normalized");
 });
 
-test("results are normalized for one double and one integer that are normalized subtracted (0, 1.0, -)", () => {
+test("results return normalized for states using e that is normalized (1/e, sqrt(1 - 1/e^2), +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("0", "1.0", false);
+  const resultPlus = checkNormalization("1/e", "sqrt(1 - 1/e^2)", true);
   // Make sure checkNormalization returns normalized
-  expect(result.resultOfCheck).toBe("normalized");
+  expect(resultPlus.resultOfCheck).toBe("normalized");
+  const resultMinus = checkNormalization("1/e", "sqrt(1 - 1/e^2)", false);
+  expect(resultMinus.resultOfCheck).toBe("normalized");
+});
+
+test("results return normalized for states using E that is normalized (1/E, sqrt(1 - 1/E^2), +/-)", () => {
+  // + = true, - = false. Check normalization expects strings from
+  // the user typing
+  const resultPlus = checkNormalization("1/E", "sqrt(1 - 1/E^2)", true);
+  // Make sure checkNormalization returns normalized
+  expect(resultPlus.resultOfCheck).toBe("normalized");
+  const resultMinus = checkNormalization("1/E", "sqrt(1 - 1/E^2)", false);
+  expect(resultMinus.resultOfCheck).toBe("normalized");
+});
+
+// List of functions:
+/**
+ * "cos", "sin", "tan", "cot", "sec", "csc", "asin", "acos",
+  "atan",
+  "sqrt",
+  "acot",
+  "asec",
+  "acsc",
+  "sinh",
+  "cosh",
+  "tanh",
+  "asinh",
+  "acosh",
+  "atanh",
+  "nthRoot",
+  "exp",
+  "log",
+  "pow",
+  "abs",
+  "arg",
+  "conj",
+  "re",
+  "im",
+  "complex",
+ */
+test("results are normalized for valid cos uses (cos(pi/2), 1.0, +/-)", () => {
+  // + = true, - = false. Check normalization expects strings from
+  // the user typing
+  const resultPlus = checkNormalization("0", "1.0", true);
+  // Make sure checkNormalization returns normalized
+  expect(resultPlus.resultOfCheck).toBe("normalized");
+  const resultMinus = checkNormalization("0", "1.0", false);
+  expect(resultMinus.resultOfCheck).toBe("normalized");
 });
 
 // ================================================================
@@ -298,18 +321,12 @@ test("helper function returns correct probs and incorrect norm on input that is 
 /**
  * Main function tests: edge cases
  */
-test("results are normalized for less than 10^-11 margin added (0.000001, 1, +)", () => {
+test("results are normalized for less than 10^-11 margin (0.000001, 1, +/-)", () => {
   // + = true, - = false. Check normalization expects strings from
   // the user typing
-  const result = checkNormalization("0.000001", "1", true);
-  // Make sure checkNormalization returns normalized even though its not
-  expect(result.resultOfCheck).toBe("normalized");
-});
-
-test("results are normalized for less than 10^-11 margin subtracted (0.000001, 1, -)", () => {
-  // + = true, - = false. Check normalization expects strings from
-  // the user typing
-  const result = checkNormalization("0.000001", "1", false);
-  // Make sure checkNormalization returns normalized even though its not
-  expect(result.resultOfCheck).toBe("normalized");
+  const resultPlus = checkNormalization("0.000001", "1", true);
+  // Make sure checkNormalization returns normalized even though it "shouldn't" be
+  expect(resultPlus.resultOfCheck).toBe("normalized");
+  const resultMinus = checkNormalization("0.000001", "1", false);
+  expect(resultMinus.resultOfCheck).toBe("normalized");
 });
